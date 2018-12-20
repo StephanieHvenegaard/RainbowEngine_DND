0939f4a27aaf568e0908a9795bf4aa138db3f0d7
StephanieNigth <s.hvenegaard@outlook.com>
2018-12-21 00:14:54 +0100

    - Initial -

---
+++ src/the_nights/rainbow_engine/dnd/labyrinth/CompassDirection.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package the_nights.rainbow_engine.dnd.labyrinth;
+
+/**
+ *
+ * @author Stephanie
+ */
+public enum CompassDirection {
+    N("north", 0, -1), S("south", 0, 1), E("east", 1, 0), W("west", -1, 0);
+    public final String direction; //representation of direction. 
+    public final int dx;  // Direction in the array out of the x axis 
+    public final int dy;  // Direction in the array out of the y axis
+    public CompassDirection opposite;  // deklares the opposite direction for ref. 
+    public CompassDirection left;
+    public CompassDirection right;
+    // use the static initializer to resolve forward references
+    static {
+        N.opposite = S;
+        S.opposite = N;
+        E.opposite = W;
+        W.opposite = E;
+        N.left = W;
+        S.left = E;
+        E.left = N;
+        W.left = S;
+        N.right = E;
+        S.right = W;
+        E.right = S;
+        W.right = N;
+    }
+    private CompassDirection(String dir, int dx, int dy) {
+        this.direction = dir;
+        this.dx = dx;
+        this.dy = dy;
+    }
+};
+
---
+++ src/the_nights/rainbow_engine/dnd/labyrinth/Generator.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package the_nights.rainbow_engine.dnd.labyrinth;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ *
+ * @author Stephanie
+ */
+public class Generator {
+
+    private void startMazeGeneration() {
+        maze[0][0].setIsExit(true);
+        Direction dir = Direction.S;
+        // Add Exit to Current Room
+        int nextX = dir.dx;      // gets X for the next Room 
+        int nextY = dir.dy;      // gets Y for the nex Room
+        maze[0][0].setExit(dir.direction, maze[nextX][nextY]);
+        // Add oppesite exit to next Room to bind them together
+        maze[nextX][nextY].setExit(dir.opposite.direction, maze[0][0]);
+        // goes to next Cell and start the proces over again untill all rooms is full.
+        generateMaze(nextX, nextY);
+    }
+
+    public Labyrinth generateMaze(int currentX, int currentY) {
+        
+        
+        Direction[] dirs = Direction.values(); // gets all 4 enums for direction
+        Collections.shuffle(Arrays.asList(dirs)); // mixes them up to get a reandom direction.
+        for (Direction dir : dirs) // loops through each posible direction
+        {
+
+            int nextX = currentX + dir.dx;      // gets X for the next Room 
+            int nextY = currentY + dir.dy;      // gets Y for the nex Room
+            if (nextX == 0 && nextY == 0) {
+                continue; //checks if Spawn room if true skip.
+            }
+
+            if (isInsideMaze(nextX, this.size) // checks if x is indside maze.
+                    && isInsideMaze(nextY, this.size)) // checks if y is indside maze.
+            {
+                // checks if room has no exits 
+                if (!maze[nextX][nextY].hasAnyExits()) {
+
+                    // Add Exit to Current Room
+                    maze[currentX][currentY].setExit(dir.direction, maze[nextX][nextY]);
+                    // Add oppesite exit to next Room to bind them together
+                    maze[nextX][nextY].setExit(dir.opposite.direction, maze[currentX][currentY]);
+                    // goes to next Cell and start the proces over again untill all rooms is full.
+                    generateMaze(nextX, nextY);
+
+                } else {
+                    // we have reached a room with an exit, checks if we should punch a hole 
+                    // before backtracking to make sure the map is a loop
+                    if (!maze[nextX][nextY].hasExit(dir.opposite.direction) // is there allready a exit to this room
+                            && (!isLoop || ((int) (Math.random() * 20) == 5))) // force or random
+                    {
+                        // Add Exit to Current Room
+                        maze[currentX][currentY].setExit(dir.direction, maze[nextX][nextY]);
+                        // Add oppesite exit to next Room to bind them together
+                        maze[nextX][nextY].setExit(dir.opposite.direction, maze[currentX][currentY]);
+                        // the maze is now a loop
+                        isLoop = true;   // no need to force no more                     
+                    }
+                }
+            }
+        }
+    }
+
+    /*
+     * Checks if the next room is inside the maze.
+     */
+    private boolean isInsideMaze(int v, int upper) {
+        return (v >= 0) && (v < upper);
+    }
+}
